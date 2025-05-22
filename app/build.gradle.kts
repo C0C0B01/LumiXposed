3d2e84213c1f38ce12dc44345cad6f71aa44a7cb
cocobo1 <cocobotheone@gmail.com>
2025-05-22 19:12:49 +0300

    lumi edits

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "dev.lumi.xposed"
         minSdk = 26
         targetSdk = 35
-        versionCode = 0010
+        versionCode = 1
         versionName = "0.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
