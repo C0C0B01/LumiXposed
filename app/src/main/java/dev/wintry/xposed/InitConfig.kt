cb9022defb6caf746a51158694775ad38750d46e
cocobo1 <cocobotheone@gmail.com>
2025-05-26 22:51:00 +0300

    asasasasasasasasa

--- app/src/main/java/dev/wintry/xposed/InitConfig.kt
+++ app/src/main/java/dev/wintry/xposed/InitConfig.kt
@@ -15,7 +15,7 @@ data class InitConfig(
     val safeMode: Boolean = false,
 ) {
     companion object {
-        const val DEFAULT_BASE_URL = "https://github.com/pylixonly/wintry/releases/latest/download/"
+        const val DEFAULT_BASE_URL = "https://raw.githubusercontent.com/C0C0B01/Lumi/refs/heads/main/dist/bundle.js"
         val Current: InitConfig by lazy {
             try {
                 val file = File(wintryDir, "init_config.json")
